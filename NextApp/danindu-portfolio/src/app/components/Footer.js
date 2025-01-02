03519493cb378f10216646b3442f43726ced758f
Danindu Marasinghe <danindum@outlook.com>
2025-01-02 00:20:06 -0500

    Create footer

--- NextApp/danindu-portfolio/src/app/components/Footer.js
+++ NextApp/danindu-portfolio/src/app/components/Footer.js
@@ -1,76 +1,40 @@
 
 export default function Footer() {
     return (
-        <footer className="bg-dark footer-section" id="contact">
-            <div className="section" style={{ paddingBottom: "40px", paddingTop: "0px" }}>
-                <div className="container">
-                {/* Section title */}
-                <div className="row">
-                    <h2 className="section-title" style={{ color: "white" }}>
-                    Contact Info
-                    </h2>
-                </div>
-
-                {/* Contact Details */}
-                <div className="row">
-                    <div className="col-md-4">
-                    <h5 className="text-light">Email</h5>
-                    <a href="mailto:danindum@outlook.com">
-                        <p className="text-white paragraph-lg font-secondary">danindum@outlook.com</p>
-                    </a>
-                    </div>
-                    <div className="col-md-4">
-                    <h5 className="text-light">Phone</h5>
-                    <a href="tel:16476324584">
-                        <p className="text-white paragraph-lg font-secondary">+1 (647) 632-4584</p>
-                    </a>
-                    </div>
-                    <div className="col-md-4">
-                    <h5 className="text-light">Location</h5>
-                    <a
-                        href="https://www.google.ca/maps/place/Brampton,+ON/@43.7248485,-79.8996272,11z/data=!3m1!4b1!4m5!3m4!1s0x882b15eaa5d05abf:0x352d31667cc38677!8m2!3d43.7315479!4d-79.7624177"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <p className="text-white paragraph-lg font-secondary">Brampton, ON</p>
-                    </a>
-                    </div>
-                </div>
-
-                {/* Social Media Icons */}
-                <div className="row">
-                    <div className="col-sm-2"></div>
-                    <div className="col-lg-8 text-center">
-                    <a href="https://github.com/danindugit" className="fa custom-icon fa-github" target="_blank" rel="noopener noreferrer"></a>
-                    <a
-                        href="https://www.linkedin.com/in/danindu-marasinghe-ab94051a6/"
-                        className="fa custom-icon fa-linkedin"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    ></a>
-                    <a
-                        href="https://www.facebook.com/profile.php?id=100010162722755"
-                        className="fa custom-icon fa-facebook"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    ></a>
-                    <a
-                        href="https://www.instagram.com/danindu_mara01/"
-                        className="fa custom-icon fa-instagram"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    ></a>
-                    <a
-                        href="https://twitter.com/dan_witha_twist"
-                        className="fa custom-icon fa-twitter"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    ></a>
-                    </div>
-                    <div className="col-sm-2"></div>
-                </div>
-                </div>
-            </div>
-        </footer>
-    );
+        <div className="container">
+            <footer className="py-3 my-4">
+                <hr/>
+                <ul className="nav justify-content-center list-unstyled d-flex">
+                    <li>
+                        <a className="text-body-secondary" href="mailto:danindum@outlook.com">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16">
+                                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li className="ms-5">
+                        <a className="text-body-secondary" href="https://github.com/danindugit" target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li className="ms-5">
+                        <a className="text-body-secondary" href="https://www.linkedin.com/in/danindu-marasinghe-ab94051a6/" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
+                            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+                        </svg>
+                        </a>
+                    </li>
+                    <li className="ms-5">
+                        <a className="text-body-secondary" href="https://www.facebook.com/profile.php?id=100010162722755" target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
+                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </footer>
+        </div>
+  );
 }
